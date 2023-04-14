0754b650ae72be7ae263643dbb577c0f6d5fae53
AlexandrovaElisabeth <119954939+AlexandrovaElisabeth@users.noreply.github.com>
2023-04-14 15:38:01 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -27,19 +27,15 @@ class TPQueue {
             throw std::string("Full");
         } else {
             int temp = first;
-            if (isEmpty() || value.prior > arr[last-1].prior) {
-                temp = last;
-            } else {
-                while (temp != last && value.prior <= arr[temp].prior) {
-                    temp = (++temp) % size;
-                }
-                for (int i = last; i != temp; i = (size + i - 1) % size) {
-                    arr[(i + 1) % size] = arr[i];
-                }
-                ++count;
-                last = (last + 1) % size;
-                arr[temp] = value;
+            while (temp != last && value.prior < arr[temp].prior) {
+                temp = (++temp) % size;
             }
+            for (int i = last; i != temp; i = (size + i - 1) % size) {
+                arr[(i + 1) % size] = arr[i];
+            }
+            ++count;
+            last = (last + 1) % size;
+            arr[temp] = value;
         }
     }
     const T& pop() {
